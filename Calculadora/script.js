bac3b04b0be906e6f0210b028d7fba6b2c053c7c
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-09-02 22:45:48 -0300

    toggler

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -28,3 +28,13 @@ buttons.forEach((button) =>{
     button.addEventListener("click", e => calculate(e.target.dataset.value));
 
 });
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".container");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
