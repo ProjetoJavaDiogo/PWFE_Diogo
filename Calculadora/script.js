22f533f31f58ef515835eb09ce0976bc8a028830
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-09-07 00:18:08 -0300

    script

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,63 +1,44 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+", "="];
+const specialChars = ["%", "*", "/", "-", "+", "=","x²"];
 let output = "";
 
 const calculate = (btnValue) => {
-    if (btnValue === "=" && btnValue !== "") {
-        output = evaluateExpression(output.replace("%", "/100"));
+    if (btnValue === "=" && output !== "") {
+        output = evalExpression(output.replace("%", "/100"));
     } else if (btnValue === "CE") {
         output = "";
     } else if (btnValue === "DEL") {
-        output = output.slice(0, -1);
+        output = output.toString().slice(0, -1);
+    } else if (btnValue === "x²") {
+        output = Math.sqrt(parseFloat(output));
     } else {
         output += btnValue;
     }
     display.value = output;
 };
 
-const evaluateExpression = (expression) => {
-    let operators = ['*', '/', '+', '-'];
-    let values = expression.split(/[\+\-\*\/]/);
+const evalExpression = (expression) => {
+    const operators = /[+\-*/]/g;
+    const parts = expression.split(operators);
+    const operator = expression.match(operators);
 
-    let result = parseFloat(values[0]);
-
-    for (let i = 1; i < values.length; i++) {
-        let operator = expression[values[i].length];
-        let value = parseFloat(values[i]);
-
-        switch (operator) {
-            case '*':
-                result *= value;
-                break;
-            case '/':
-                result /= value;
-                break;
-            case '+':
-                result += value;
-                break;
-            case '-':
-                result -= value;
-                break;
+    if (parts.length === 2 && operator) {
+        const [num1, num2] = parts.map(parseFloat);
+        switch (operator[0]) {
+            case "+": return num1 + num2;
+            case "-": return num1 - num2;
+            case "*": return num1 * num2;
+            case "/": return num1 / num2;
+            case "x²": return Math.sqrt(num1);
+            default: return NaN;  
         }
+        
     }
 
-    return result;
+    return NaN;
 };
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
-
-
-//código para alternar entre os modos de cor
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".container");
-const toggleIcon = document.querySelector(".toggler-icon");
-
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
