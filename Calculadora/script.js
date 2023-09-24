f1d02c2b43e02313694a719d2170ff1466043b7f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-09-24 13:28:15 -0300

    script

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,52 +1,93 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+", "=","x²"];
-let output = "";
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = ""; // output é let pois os valores recebidos variam
+
+let optionsVisible = true;
+
+const toggleOptionsBtn = document.querySelector(".toggle-options");
+toggleOptionsBtn.addEventListener("click", toggleOptions);
+
+function toggleOptions() {
+    const additionalOptions = document.querySelectorAll(".additional-option");
+    optionsVisible = !optionsVisible;
+
+    additionalOptions.forEach(option => {
+        option.style.display = optionsVisible ? "inline-block" : "none";
+    });
+
+    toggleOptionsBtn.textContent = optionsVisible ? "Calculadora Normal" : "Calculadora Cientifica";
+}
 
 
 const calculate = (btnValue) => {
-    if (btnValue === "=" && output !== "") {
-        output = avalExpression(output.replace("%", "/100"));
+    if (btnValue === "=" && output !== "") { //compara os valores levando em conta o valor e o tipo de dado
+        output = evalExpression(output.replace("%", "/100"));
     } else if (btnValue === "CE") { //apagar as informações do display
         output = "";
     } else if (btnValue === "DEL") {  //apagar a primeira informação do display
         output = output.toString().slice(0, -1); 
-    } else if (btnValue === "x²") {
+    } else if (btnValue === "√") {
         output = Math.sqrt(parseFloat(output));
+    } else if(btnValue === "pi"){
+        output = Math.PI;
+    } else if(btnValue === "e"){
+        output = Math.E;
+    } else if(btnValue === "fat"){
+        output = calcularFatorial(parseFloat(output));
+    } else if(btnValue === "log"){
+        output = calcularLogaritmo(10, parseFloat(output)); 
     } else {
-        output += btnValue;
+        output += btnValue; //o valor do botão é adicionado ao display
     }
     display.value = output; //aparecer no display o output
 };
 
-// declarando a função "avalExpression"
-const avalExpression = (expression) => {
-    const operators = /[+\-*/]/g; // corresponde aos operadores +, -, *, /
+//função para calcular o fatorial
+const calcularFatorial = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * calcularFatorial(n - 1);
+    }
+};
+//função para calular log
+const calcularLogaritmo = (base, numero) => {
+    return Math.log(numero) / Math.log(base);
+};
+
+
+// função "avalExpression"
+const evalExpression = (expression) => {
+    const operators = /[+\-*/]/g; // corresponde aos operadores matemáticos +, -, *, /
     const parts = expression.split(operators); // usado em strings para dividir a string em um array de substrings, usando um separador nesse caso os operadores
+    //exemplo: 10+20 ==> ("10","20")
     const operator = expression.match(operators); // usando o match para encontrar todos os operadores
 
     if (parts.length === 2 && operator) {  //verifica se a string foi divida corretamente (nº e operator) e se existe um operador
         const [num1, num2] = parts.map(parseFloat); //extrai os dois valores e converte para float
+        //cria um novo array a partir de um array existente, aplicando uma função a cada elemento do array original
 
         switch (operator[0]) { //usando switch case para definir os casos das operações matemáticas
             case "+": return num1 + num2;
             case "-": return num1 - num2;
             case "*": return num1 * num2;
-            case "/": 
-            if (num2 !== 0) {
-                return num1 / num2;
-            } else {
-                return "Erro: / por 0";
-            }
-            case "x²": return Math.sqrt(num1);
+            case "/": return num2!==0 ? num1/num2 : "Erro: / por 0"; // operador ternário 
             //adicionar mais operações
             default: return NaN;  
         }
         
-    }
-
-    return NaN; //else da expressão
+    } 
+    return NaN;
 };
+
+// const evalExpression = (expression) => {
+//     try {
+//         return eval(expression);
+//     } catch (error) {
+//         return "Erro";
+//     }
+// };
 
 buttons.forEach((button) => { 
     button.addEventListener("click", (e) => calculate(e.target.dataset.value));
